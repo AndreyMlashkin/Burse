55d170f7a14d2f549baf6d4cf0587d1bdeaf1a5e
AndreyMlashkin <malashkin.andrey@mail.ru>
2014-12-13 13:48:06 +0300

    graphics now have pretty view

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,11 +13,12 @@ MainWindow::MainWindow(QWidget *parent) :
     m_plotter(new QCustomPlot())
 {    
     m_ui->setupUi(this);
-   // m_plotter->resize(500, 500);
     centralWidget()->layout()->addWidget(m_plotter);
     m_plotter->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
 
     connect(m_ui->add, SIGNAL(clicked()), SLOT(addTransaction()));
+
+    initGraphics();
 }
 
 MainWindow::~MainWindow()
@@ -44,12 +45,32 @@ Transaction *MainWindow::formTransaction() const
 void MainWindow::initGraphics()
 {
     m_plotter->axisRect()->setupFullAxesBox();
-
     m_plotter->plotLayout()->insertRow(0);
-    m_plotter->plotLayout()->addElement(0, 0, new QCPPlotTitle(m_plotter, tr("Область существования параметров самолета")));
-
+    m_plotter->plotLayout()->addElement(0, 0, new QCPPlotTitle(m_plotter, tr("Торговая сессия")));
 
     m_plotter->xAxis->setLabel("Время");
     m_plotter->yAxis->setLabel("Цена");
     m_plotter->legend->setVisible(true);
+
+
+    QPen graphPen;
+    graphPen.setColor(Qt::black);
+    graphPen.setWidthF(3);
+
+    m_plotter->addGraph();
+    m_plotter->graph(0)->setLineStyle(QCPGraph::lsLine);
+    m_plotter->graph(0)->setName("Цена");
+    m_plotter->graph(0)->setPen(graphPen);
+
+    m_plotter->addGraph();
+    graphPen.setColor(Qt::blue);
+    m_plotter->graph(1)->setLineStyle(QCPGraph::lsLine);
+    m_plotter->graph(1)->setName("Спрос");
+    m_plotter->graph(1)->setPen(graphPen);
+
+    m_plotter->addGraph();
+    graphPen.setColor(Qt::red);
+    m_plotter->graph(2)->setLineStyle(QCPGraph::lsLine);
+    m_plotter->graph(2)->setName("Предложение");
+    m_plotter->graph(2)->setPen(graphPen);
 }
